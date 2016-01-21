ae8d3ba377cc6fa92a9d60f19602536a27e58ab6
Suraj Pathak <freesuraj@gmail.com>
2016-01-21 11:25:45 +0800

    Fix the last modified date check issue

--- TLDR/Remote.swift
+++ TLDR/Remote.swift
@@ -10,6 +10,10 @@ import Foundation
 import SSZipArchive
 
 struct NetworkManager {
+    
+    static func cachedZipUrl() -> String {
+        return tldrZipUrl
+    }
 
     static func remoteZipUrl() -> String {
         guard let gitUrl = remoteConfig["gitUrl"],
@@ -28,19 +32,34 @@ struct NetworkManager {
             updateTldrLibrary()
             return
         }
-        let timeIntervalSinceLastUpdate = NSDate().timeIntervalSince1970 - localLastUpdateTime
-        let forceUpdateInterval = Double(5 * 24 * 60 * 60) // i.e. 5 days
-        if  timeIntervalSinceLastUpdate > forceUpdateInterval {
-            print("The update was too long ago. Updating a new version now.")
-            updateTldrLibrary()
-        } else {
-            print("Current version was updated very recently. Not auto updating now.")
+        // Do a head request to see if remote is updated since this date
+        let source = cachedZipUrl()
+        guard let url = NSURL(string: source) else {
+            return
         }
+        let request = NSMutableURLRequest(URL: url)
+        request.HTTPMethod = "HEAD"
+        request.addValue(localLastUpdateTime.stringInHeaderFormat(), forHTTPHeaderField: "If-Modified-Since")
+        let session = NSURLSession.sharedSession()
+        let task = session.dataTaskWithRequest(request) { data, response, error in
+            guard let httpResponse = response as? NSHTTPURLResponse else {
+                print("The last modified date could not be found. Updating a new version now anyway.")
+                updateTldrLibrary()
+                return
+            }
+            if httpResponse.statusCode == 304 {
+                print("Current version was updated very recently. Not auto updating now.")
+            } else {
+                print("The update was too long ago. Updating a new version now.")
+                updateTldrLibrary()
+            }
+        }
+        task.resume()
     }
 
     static func updateTldrLibrary() {
         print("Updating tldr library. This might take few seconds.")
-        let zipSource = remoteZipUrl()
+        let zipSource = cachedZipUrl()
         dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_HIGH, 0), {
             guard let url = NSURL(string: zipSource),
                 let data = NSData(contentsOfURL: url) else {
@@ -50,12 +69,12 @@ struct NetworkManager {
             if data.writeToFile(FileManager.urlToTldrUpdateFolder()!.path!, atomically: true) {
                 let destinationUrl = FileManager.urlToTldrUpdateFolder()!.URLByDeletingPathExtension!
                 if SSZipArchive.unzipFileAtPath(FileManager.urlToTldrUpdateFolder()!.path!, toDestination: destinationUrl.path!) {
-                    updateLastUpdateDate()
                     print("Zip is downloaded, unzipped and saved")
                     do {
                         try FileManager.fileManager.removeItemAtURL(FileManager.urlToTldrUpdateFolder()!)
                     } catch {}
                     FileManager.copyFromSourceUrl(destinationUrl, to: FileManager.urlToTldrFolder(), replaceIfExist: true)
+                    updateLastUpdateDate()
                 }
             }
         })
@@ -65,10 +84,19 @@ struct NetworkManager {
         NSUserDefaults.standardUserDefaults().setObject(NSNumber(double: NSDate().timeIntervalSince1970), forKey: "LastModifiedDate")
     }
 
-    static func getLastModifiedDate() -> NSTimeInterval? {
+    static func getLastModifiedDate() -> NSDate? {
         guard let date = NSUserDefaults.standardUserDefaults().valueForKey("LastModifiedDate") as? Double else {
             return nil
         }
-        return date
+        return NSDate(timeIntervalSince1970: date)
+    }
+}
+
+extension NSDate {
+    func stringInHeaderFormat() -> String {
+        let formatter = NSDateFormatter()
+        formatter.dateFormat = "EEE, dd MMM yyyy HH:mm:ss z"
+        formatter.timeZone = NSTimeZone(name: "GMT")
+        return formatter.stringFromDate(self) // eg Wed, 20 Jan 2016 23:15:28 GMT
     }
 }
